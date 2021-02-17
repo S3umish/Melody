97698dcb2b55815d2b1b8078dc286ababff997bb
sunitee <suniteebajpai2@gmail.com>
2021-02-17 18:44:01 -0500

    updated instruments controller

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,11 +1,18 @@
 class EnrollmentsController < ApplicationController
 
     def index
-
+        # binding.pry
+        if params[:instrument_id]
+            @instrument = current_user.instruments.find_by(id: params[:instrument_id])
+            @lessons =Instrument.find(params[:instrument_id]).enrollments
+            @lessons = @instrument.enrollments
+        else
+            @lessons = current_user.enrollments
+        end
     end
 
     def show
-        
+
     end
 
 end
--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,12 +1,34 @@
 class InstrumentsController < ApplicationController
     def index
         @instruments = Instrument.all
-      end
+    end
           
-      def new
+    def new
         @instrument = Instrument.new
-        3.times{@instrument.lessons.build}
-      end
-        
+        3.times{@instrument.enrollments.build}
+    end
 
+    def create
+        @instrument = current_user.instruments.build(instrument_params)
+        if @instrument.save
+          redirect_to instruments_path(@instrument)
+        else
+          render :new
+        end
+    end
+        
+    def show
+        @instrument = current_user.instruments.find_by(id: params[:id])
+    end
+      
+    def destroy
+        @instrument.destroy
+        redirect_to instruments_path
+    end
+      
+    private
+    def instrument_params
+        params.require(:instrument).permit(:name, enrollments_attributes: [:student,:start_date, :level, :price, :duration))
+    end   
+        
 end
