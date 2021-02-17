853b5d95be46246f9b13509c380b3547156bcf8d
sunitee <suniteebajpai2@gmail.com>
2021-02-17 18:05:11 -0500

    added controller files and setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  root to: 'sessions#welcome'
+  
+  get '/login',  to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
+
+  resources :users
+  resources :enrollments
+  resources :instruments do 
+    resources :enrollments, only: [:new, :index, :create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
