66a4910321e334295ac8a032b1bf91e2d266f986
sunitee <suniteebajpai2@gmail.com>
2021-02-19 15:16:30 -0500

    fixed validation for username and email and userexperience

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
     has_many :enrollments
     has_many :instruments, through: :enrollments
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
 
     has_secure_password
 end
