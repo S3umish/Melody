885c38670397b0789c6251b0e4a496442a25951e
sunitee <suniteebajpai2@gmail.com>
2021-02-19 15:46:59 -0500

    updating views

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -3,7 +3,6 @@ class Instrument < ApplicationRecord
     has_many :users, through: :enrollments
     validates :name, presence: true, uniqueness: true
     
-
     accepts_nested_attributes_for :enrollments,reject_if: proc {|attr| attr['level'].blank? || attr['price'].blank?|| attr['duration'].blank?}
     
     scope :alphabetize, -> { order(name: :asc) }
